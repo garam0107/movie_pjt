ee0e0f2ab6bf09908673cd026609117c8efdb736
Yujiin <jiin4083@naver.com>
2024-11-18 11:45:01 +0900

    중간점검

---
+++ front/final_front/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from '@/components/movie/common/Footer.vue';
+Footer
+</script>
+
+<template>
+
+      <nav>
+        <RouterLink to="/main">MovieMain</RouterLink>
+      </nav>
+
+  <RouterView />
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ front/final_front/src/views/MovieMainView.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>영화 메인</h1>
+</template>
+
+<script setup>
+import MovieMain from '@/components/movie/MovieMain.vue';
+import { useMovieStore } from '@/stores/counter';
+import { onMounted } from 'vue';
+
+const movies = useMovieStore()
+onMounted(() => {
+  movies.getMovies()
+})
+</script>
+
+<style scoped>
+
+</style>
