f63fdba504ef5e220a454d747a40f296c4fb69a6
garam0107 <name3937@gmail.com>
2024-11-19 10:32:33 +0900

    url 수정

--- back/diaries/views.py
+++ back/diaries/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view,permission_classes
 # Create your views here.
+
+def user_diary(request, user_pk):
+    pass
