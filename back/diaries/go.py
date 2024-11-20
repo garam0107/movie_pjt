58a2f8f76ccac4bf800d5a45948c816e2f91f907
garam0107 <name3937@gmail.com>
2024-11-21 01:24:10 +0900

    json에러

--- back/diaries/go.py
+++ back/diaries/go.py
@@ -1,18 +1,29 @@
 import os
 import json
-print(f'현재 작업 디렉토리:{os.getcwd()}')
+import re
+
 
 from pathlib import Path
 
+
 # 절대 경로로 변환해서 확인
-json_file_path = Path(__file__).resolve().parent/'updated_movies.json'
-print(f"확인할 파일 경로: {json_file_path}")
-if not json_file_path.exists():
-    print("Error: 파일이 존재하지 않습니다.")
-else:
-    print("파일이 정상적으로 존재합니다.")
+# json_file_path = Path(__file__).resolve().parent/'updated_movies.json'
+# print(f"확인할 파일 경로: {json_file_path}")
+# if not json_file_path.exists():
+#     print("Error: 파일이 존재하지 않습니다.")
+# else:
+#     print("파일이 정상적으로 존재합니다.")
 
+# with open(json_file_path, encoding='utf-8') as f:
+#     available_movies = json.load(f)
 
+# print(available_movies)
+
+# json_file_path2 = Path(__file__).resolve().parent.parent / 'movies' / 'fixtures' / 'movies.json'
+
+# with open(json_file_path2, encoding='utf-8') as f:
+#     available_movies2 = json.load(f)
+# print(available_movies2)
 # import json
 # from pathlib import Path
 
@@ -59,3 +70,6 @@ else:
 # # 수정된 JSON 데이터를 파일로 저장
 # with open('updated_movies.json', 'w', encoding='utf-8') as f:
 #     json.dump(movies, f, ensure_ascii=False, indent=4)
+from diaries.models import Diary
+diary = Diary.objects.get(id=18)  # 특정 다이어리 가져오기
+print(diary.recommend_reasons)  # JSONField 값 확인
