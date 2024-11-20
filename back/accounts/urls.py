71e16f8937c3766694f407aa062839e684e0ab97
garam0107 <name3937@gmail.com>
2024-11-20 22:21:42 +0900

    gpt 추천

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('<int:user_pk>/mypage/', views.mypage), # 마이 페이지
+    path('<str:user_username>/mypage/', views.mypage), # 마이 페이지
     path('<int:user_pk>/follow/', views.follow), # 팔로우 기능, 팔로우한 사람의 정보 가져오기)
     path('<int:user_pk>/update/', views.update), # 회원 정보 수정
 ]
